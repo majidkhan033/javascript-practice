83efcc06588de6fedfe49a8e350a3f313fdd932d
Abdul Majid Khan <kmajid748@gmail.com>
2022-07-25 12:02:15 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,100 @@
+// Control Flow
+// 1. Sequential
+// 2. Conditions
+// 3. Iterations
+// 4. Functions
+// 5. OOPs
+
+// Conditions
+// 1. if conditions
+// 2. if - else condition
+// 3. if - else if - else conditions
+// 4. Nested conditions 
+
+
+// 1. if conditions
+// if (condition) {
+    // statement 
+// }
+
+
+// example 1
+// if (1) { console.log("I am one")}
+// if (true) {console.log("I am one")}
+
+
+// example 2
+// if (0) { console.log("I am one")}
+// if ('') { console.log("I am one")}
+// if('zero') { console.log("I am one")}
+// if (0.0) { console.log("I am one")}
+// if ('null') { console.log("I am two")}
+// if (null) { console.log("I am three")}
+
+
+// example 3
+// if (0) { console.log("I am one")}
+// if ([]) { console.log("I am ozone")}
+// if ('') { console.log("I am tone")}
+// if (null) { console.log("I am none")}
+// if (0.0) { console.log("I am gone")}
+
+
+// example 4
+// if(3<4){
+//     console.log('monkey')
+// }
+
+
+// example 5
+// if((0) && '[]') {
+//     console.log('hey')
+// }
+// if (8&7){
+//     console.log('hey1')
+// }
+
+
+// example 6
+// if( [1,2] == [1,2]){
+//     console.log('hey1')
+// }
+
+// if ((2 == '2') | ((2) == 2.0)) {
+//     console.log('hey2')
+// }
+
+
+// example 7
+// if ((4^2) << (6|7)){
+//     console.log('hey2')
+// }
+
+// var a=3, b=5
+// if (a&b | b|a) {
+//     console.log('hey3')
+// }
+
+
+// example 8
+// var a=5, b=7
+// if (a>=b & a<b){
+//     console.log('hey')
+// }
+
+// if ('sahana' == 'sahana'){
+//     console.log('hey1')
+// }
+// if('Sahana' == 'sahana'){
+//         console.log('hey3')
+// }
+// if('Sahana' === 'sahana'){
+//     console.log('hey4')
+// }
+// if('Sahana' === 'Sahana'){
+//     console.log('hey5')
+// }
+
+
+
+
