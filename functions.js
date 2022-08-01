ba3c81b9b146d80ed795f0ceabc5169d8f241343
Abdul Majid Khan <kmajid748@gmail.com>
2022-08-01 11:38:41 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,93 @@
+// Functions
+
+// Syntax -
+// function function_name(parameters){
+//     Task/statements
+// }
+
+// Function definitions
+//function_name(arguements) //function call back
+
+// add 2 numbers using functions
+// function add(){
+//     console.log(2+3)
+// }
+// add ()
+
+// function add(){
+//     console.log(2+3)
+// }
+// console.add(add())
+
+
+// function add (a,b){
+//         // console.log(a)
+//         // console.log(b)
+//         console.log(a+b)
+// }
+// // add()
+// add(7,7)
+// add(6,7)
+// add(8,19)
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// even or odd using functions
+
+// function even(num){
+//     for (i=5;i<=15;i++){
+//     if ((num%2)==0){
+//         console.log(`${num} is even`)
+//     }
+// } even(i)
+// }
+
+//------------------------------------------------------------------------------------------------------------------------------------
+
+// wap to find max number in an Array
+
+// a = {'a':1, b:2, b:3, 'c':4}
+// console.log(a)
+// console.log(Object.keys(a))
+// console.log(Object.values(a))
+// console.log(a[b])
+// console.log(a['b'])
+// console.log(a.b)
+
+// updating the key value
+// a['b'] = 4
+// console.log(a)
+// console.log(a['b'])
+
+//adding an item
+// a['d'] = 6
+// console.log(a)
+
+// //removal of an item
+// delete a['b']
+// console.log(a)
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+//WAP to find the biggest/greatest number in the given array?
+
+a = [1,2,3,4,5]
+// console.log(a.length)
+// console.log(Math.max(a))
+// function greatest(a){
+//     Math.max(a)
+// }
+// console.log(a)
+
+
+// let a = [2,4,6,7,3]
+// function greatest(a){
+//     temp = a[0];
+//     for (i=1;i<=(a.length-1);i++){
+//         if(a[i]>temp)
+//         temp = a[i]
+//     }
+//     return temp;
+// }
+// console.log(greatest(a))
+
